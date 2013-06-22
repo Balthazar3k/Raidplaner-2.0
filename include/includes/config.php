3d1d2044aeabe01ecbdbc4837537226420cb4061
Balthazar3k <angelo.b3k@gmail.com>
2013-06-22 09:54:54 +0300

    Dataie Fehl am Platz

--- include/includes/config.php
+++ include/includes/config.php
@@ -1,7 +1,1 @@
-<?php
-define ( 'DBHOST', 'localhost' );   # sql host
-define ( 'DBUSER', 'Balthazar3k');  # sql user
-define ( 'DBPASS', 'macchia');  # sql pass
-define ( 'DBDATE', 'raidplaner-2.0');  # sql datenbank
-define ( 'DBPREF', 'prefix_'); # sql prefix
-?>
+1
