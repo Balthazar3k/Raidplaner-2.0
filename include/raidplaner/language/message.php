5f6967366f6be927edad1312e8be47e03c5561de
Balthazar3k <angelo.b3k@gmail.com>
2013-06-22 10:46:48 +0200

    Sprach und Config Files

---
+++ include/raidplaner/language/message.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ *	Mit der Zeit werden alle meldungen hier drinne stehen!
+ * 	2012 Balthazar3k!
+ * 	wenn ihr was an Rechtschreibfehler findet, so weist mich doch bitte drauf hin :)
+ */
+ 
+$raidMsg = array();
+
+/* STATUS MELDUNGEN */
+
+$raidMsg['updateBattleNetCharakter'][0] = "Sorry, aber das Update f&uuml;r <b>{name}</b> &uuml;ber Battle.net war nicht m&ouml;glich!";
+$raidMsg['updateBattleNetCharakter'][1] = "Update f&uuml;r <b>{name}</b> &uuml;ber Battle.net war erfolgreich";
+
+$raidMsg['noperm'] = "Sie habe nicht die n&ouml;tigen Rechte um diese Aktion auszuf&uuml;ren";
+$raidMsg['noArray'] = "Diese eingabe ist kein Array!";
+
+$raidMsg['save&sort'][0] = "Eintr&auml;ge konnten <b>nicht</b> ge&auml;ndert & Sortiert werden!";
+$raidMsg['save&sort'][1] = "Eintr&auml;ge wurden ge&auml;ndert & Sortiert!";
+
+$raidMsg['changeRecht'][0] = "Recht konnte <b>nicht</b> ge&auml;ndert werden!";
+$raidMsg['changeRecht'][1] = "Recht wurde erfolgreich ge&auml;ndert!";
+
+$raidMsg['changeRang'][0] = "Recht konnte <b>nicht</b> ge&auml;ndert werden!";
+$raidMsg['changeRang'][1] = "Recht wurde erfolgreich ge&auml;ndert!";
+
+$raidMsg['changeUser'][0] = "dieser Charakter konnte dem User nicht zugeteilt werden!";
+$raidMsg['changeUser'][1] = "Charakter wurde erfolgreich dem User zugeteilt";
+
+$raidMsg['moduleRights'][0] = "Module Rechte konnten <b>nicht</b> erfolgreich ge&auml;ndert werden!";
+$raidMsg['moduleRights'][1] = "Module Rechte wurden erfolgreich ge&auml;ndert.";
+
+$raidMsg['moduleLink'][0] = "Module Link konnte <b>nicht</b> ge&auml;ndert werden!";
+$raidMsg['moduleLink'][1] = "Module Link wurde gespeichert!";
+
+$raidMsg['removeCharakter'][0] = "Charakter <b>{name}</b> konnte <b>nicht</b> gel&ouml;scht werden!";
+$raidMsg['removeCharakter'][1] = "Charakter <b>{name}</b> wurde erfolgreich gel&ouml;scht!";
+
+$raidMsg['success'][0] = "Aktion war <b>nicht</b> erfolgreich!";
+$raidMsg['success'][1] = "Aktion war erfolgreich!";
+$raidMsg['success'][2] = "Achtung!";
+
+$raidMsg['remove'][0] = "L&ouml;schen war <b>nicht</b> erfolgreich!";
+$raidMsg['remove'][1] = "L&ouml;schen war erfolgreich!";
+
+$raidMsg['regist'][0] = "beim Senden der Bewerbung ist ein Fehler aufgetreten, bitte versuchen Sie es sp&auml;ter erneut!";
+
+/* TEXTE */
+$raidMsg['rekrutieren'] = "Wir suchen zur Zeite keine weiteren Mitglieder";
+$raidMsg['ilchcfg'] = "Es wurden <b>Wichtige</b> einstellungen am System ge&auml;ndert! Bitte &uuml;berpr&uuml;fen Sie diese <a href='".ilch_getRaidConfigLink()."'>Einstellungen</a> im reiter <b>Raidplaner</b>!";
+
+/* N/A
+$raidMsg['name'] = "Name";
+$raidMsg['charakter'] = "Charakter";
+$raidMsg['charaktere'] = "Charaktere";
+$raidMsg['username'] = "Username";
+$raidMsg['recht'] = "Recht";
+$raidMsg['charaktere'] = "Charaktere";
+*/
+?>
---
+++ include/raidplaner/language/spezialiesirungs.php
@@ -0,0 +1,52 @@
+<?php
+/*
+	Übersetzung von balthazar3k.funpic.de 03.10.2012
+*/
+$spezialiesirung = array();
+## DK
+$spezialiesirung['Blood'] = "Blut";
+$spezialiesirung['Frost'] = "Frost";
+$spezialiesirung['Unholy'] = "Unheilig";
+## Dudu
+$spezialiesirung['Balance'] = "Gleichgewicht";
+$spezialiesirung['Feral'] = "Wildheit-W&auml;chter";
+$spezialiesirung['Guardian'] = "Wildheit-W&auml;chter";
+$spezialiesirung['Restoration'] = "Wiederherstellung";
+## Hunter
+$spezialiesirung['Beast Mastery'] = "Tierherrschaft";
+$spezialiesirung['Marksmanship'] = "Treffsicherheit";
+$spezialiesirung['Survival'] = "&Uuml;berleben";
+## Mage
+$spezialiesirung['Arcane'] = "Arkan";
+$spezialiesirung['Fire'] = "Feuer";
+$spezialiesirung['Frost'] = "Frost";
+## Mönch
+$spezialiesirung['Brewmaster'] = "Braumeister";
+$spezialiesirung['Mistweaver'] = "Nebelwirker";
+$spezialiesirung['Windwalker'] = "Windl&auml;ufer";
+## Pala
+$spezialiesirung['Holy'] = "Heilig";
+$spezialiesirung['Protection'] = "Schutz";
+$spezialiesirung['Retribution'] = "Vergeltung";
+## Priest o. Disciplin
+$spezialiesirung['Discipline'] = "Disziplin";
+$spezialiesirung['Holy'] = "Heilig";
+$spezialiesirung['Shadow'] = "Schatten";
+## Rouge
+$spezialiesirung['Assassination'] = "Meucheln";
+$spezialiesirung['Combat'] = "Kampf";
+$spezialiesirung['Subtlety'] = "T&auml;schung";
+## Schaman
+$spezialiesirung['Elemental'] = "Elementar";
+$spezialiesirung['Enhancement'] = "Verst&auml;rkung";
+$spezialiesirung['Restoration'] = "Wiederherstellung";
+## Warlock
+$spezialiesirung['Affliction'] = "Gebrechen";
+$spezialiesirung['Demonology'] = "D&auml;monologie";
+$spezialiesirung['Destruction'] = "Zerst&ouml;rung";
+## Warri
+$spezialiesirung['Arms'] = "Waffen";
+$spezialiesirung['Fury'] = "Furor";
+$spezialiesirung['Protection'] = "Schutz";
+
+?>
